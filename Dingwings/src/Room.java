914d0f30f46e8d63b442e52410fad512c37a3aaf
Nathan McClain <nathanmcclain@ksu.edu>
2016-11-21 14:09:23 -0600

    Updated Room Constructors/Initilaization
    I added for loops that initialize the room with pathways in each direction. I still need to figure out how to add rooms

--- Dingwings/src/Room.java
+++ Dingwings/src/Room.java
@@ -7,6 +7,7 @@ public class Room
 	public Room()
 	{
 		//initialize with walls on all sides
+		/*
 		for(int x = 0; x < Main.MAP_WIDTH; x++)
 		{
 			for(int y = 0; y < Main.MAP_HEIGHT; y++)
@@ -17,6 +18,70 @@ public class Room
 					map[x][y] = Main.BLOCK_AIR;
 			}
 		}
+		*/
+		//initialize with walls on all sides but top has pathway
+		/*
+				for(int x = 0; x < Main.MAP_WIDTH; x++)
+				{
+					for(int y = 0; y < Main.MAP_HEIGHT; y++)
+					{
+						if(x==0||x==Main.MAP_WIDTH-1||y==Main.MAP_HEIGHT-1)
+							map[x][y] = Main.BLOCK_WALL;
+						else
+							map[x][y] = Main.BLOCK_AIR;
+						if (y == 0 && x!=Main.MAP_WIDTH/2)
+							map[x][y] = Main.BLOCK_WALL;
+					}
+				}
+		*/
+		
+		//initialize with walls on all sides but bottom has pathway
+		/*
+				for(int x = 0; x < Main.MAP_WIDTH; x++)
+				{
+					for(int y = 0; y < Main.MAP_HEIGHT; y++)
+					{
+						if(x==0||y==0||x==Main.MAP_WIDTH-1)
+							map[x][y] = Main.BLOCK_WALL;
+						else
+							map[x][y] = Main.BLOCK_AIR;
+						if ( y== Main.MAP_HEIGHT - 1 && x != Main.MAP_WIDTH/2)
+							map[x][y] = Main.BLOCK_WALL;
+					}
+				}
+		*/
+		
+		//initialize with walls on all sides but left has pathway
+		/*
+				for(int x = 0; x < Main.MAP_WIDTH; x++)
+				{
+					for(int y = 0; y < Main.MAP_HEIGHT; y++)
+					{
+						if(y==0||x==Main.MAP_WIDTH-1||y==Main.MAP_HEIGHT-1)
+							map[x][y] = Main.BLOCK_WALL;
+						else
+							map[x][y] = Main.BLOCK_AIR;
+						if ( x==0 && y!= Main.MAP_HEIGHT/2)
+							map[x][y] = Main.BLOCK_WALL;
+					}
+				}
+		*/
+		
+		//initialize with walls on all sides but right has pathway
+		/*
+				for(int x = 0; x < Main.MAP_WIDTH; x++)
+				{
+					for(int y = 0; y < Main.MAP_HEIGHT; y++)
+					{
+						if(x==0||y==0||y==Main.MAP_HEIGHT-1)
+							map[x][y] = Main.BLOCK_WALL;
+						else
+							map[x][y] = Main.BLOCK_AIR;
+						if ( x == Main.MAP_WIDTH - 1 && y != Main.MAP_HEIGHT/2)
+							map[x][y] = Main.BLOCK_WALL;
+					}
+				}
+		*/
 	}
 	HashMap <Integer, Room> roomMap = new HashMap <Integer, Room>();
 	// add room to hashamp (each room is predetermined
