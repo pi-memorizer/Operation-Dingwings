dcf42a6c65b38b7d4a9db037032222ead3391b1f
Unknown <bpriddle@ksu.edu>
2016-11-22 15:33:36 -0600

    Added Boss class

---
+++ Dingwings/src/Boss.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Boss extends Enemy 
+{
+	ArrayList<Chaser> chasers = new ArrayList<>(3);
+	
+	public Boss(int x, int y)
+	{
+		//health = 8, damage = 1, moveSpeed = 2
+		super(8, 1, 2);
+		super.xCoord = x;
+		super.yCoord = y;
+	}
+	
+	protected void move()
+	{
+		//move like twiddle, but randomly switches between left/right and up/down
+	}
+	
+	protected void attack()
+	{
+		//shoot projectiles in one or two patterns
+	}
+	
+	protected void spawnChasers()
+	{
+		//add three chasers into boss's arraylist
+		//will need a check somewhere for when that arraylist is empty 
+		//to spawn 3 more
+	}
+}
