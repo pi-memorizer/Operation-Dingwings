e8186159108100b306497d78735f1e99747a9512
Nathan McClain <nathanmcclain@ksu.edu>
2016-11-28 11:29:37 -0600

    Updated Room Generation

--- Dingwings/src/Coord.java
+++ Dingwings/src/Coord.java
@@ -1,3 +1,5 @@
 public class Coord {
-	//TODO implement code about player's location for hashmap
+	//TODO implement code about room's location for hashmap
+	public static int mapX = 0;
+	public static int mapY = 0;
 }
--- Dingwings/src/Room.java
+++ Dingwings/src/Room.java
@@ -3,10 +3,16 @@ import java.util.Random;
 
 public class Room
 {
-	//TODO Make a noticeable screen marking when the player changes rooms
+	public boolean exitTop;
+	public boolean exitLeft;
+	public boolean exitRight;
+	public boolean exitBottom;
+	public int currentRoomX;
+	public int currentRoomY;
 	int [][] map = new int[Main.MAP_WIDTH][Main.MAP_HEIGHT]; //The rooms map array of sprite indices
 	
 	HashMap <Coord, Room> roomMap = new HashMap <Coord, Room>();
+	Coord coord = new Coord();
 		// add room to hashamp (each room is predetermined
 		// once we enter a new room, generate room from hashmap
 	public Room()
@@ -39,6 +45,10 @@ public class Room
 						map[x][y] = Main.BLOCK_DOOR;
 				}
 			}
+			exitTop = true;
+			exitLeft = true;
+			exitBottom = true;
+			exitRight = true;
 		}
 		Random r = new Random();
 		int w = r.nextInt(8); // change this number depending on the number of options. 
@@ -49,6 +59,7 @@ public class Room
 			//this will be used to only generate rooms with openings on the left
 			//########################################################################################
 		{
+			Coord.mapX = Coord.mapX - 1;
 			Player.xCoord = 1;
 			switch(w)
 			{
@@ -67,6 +78,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = false;
 				break;
 				}
 			case 1: {//initialize with walls on all sides but bottom and left have pathway
@@ -88,6 +103,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 2: {//initialize with walls on all sides but right and left have pathway
@@ -109,6 +128,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 3: {//initialize with walls on all sides but top and left have pathway
@@ -130,6 +153,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = false;
 				break;
 				}
 			case 4: {//initialize with walls on all sides but bottom, top, and left have pathway
@@ -155,6 +182,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 5: 	{//initialize with walls on all sides but bottom, right, and left have pathway
@@ -180,6 +211,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 6: {//initialize with walls on all sides but top, right, and left have pathway
@@ -205,6 +240,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 7: {//initialize with walls on all sides but all sides have pathway
@@ -234,6 +273,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 		}
@@ -243,6 +286,7 @@ public class Room
 			//this will be used to only generate rooms with openings on the right
 			//########################################################################################
 		{
+			Coord.mapX = Coord.mapX + 1;
 			Player.xCoord = Main.MAP_WIDTH-2;
 			switch(w)
 			{
@@ -261,6 +305,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = false;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 1: {//initialize with walls on all sides but right and left have pathway
@@ -282,6 +330,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 2: {//initialize with walls on all sides but top and right have pathway
@@ -303,6 +355,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 3: {//initialize with walls on all sides but bottom and right have pathway
@@ -324,6 +380,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 4: {//initialize with walls on all sides but bottom, top, and right have pathway
@@ -349,6 +409,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 5: 	{//initialize with walls on all sides but bottom, right, and left have pathway
@@ -374,6 +438,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 6: {//initialize with walls on all sides but top, right, and left have pathway
@@ -399,6 +467,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 7: {//initialize with walls on all sides but all sides have pathway
@@ -428,6 +500,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			}
@@ -437,6 +513,7 @@ public class Room
 			//this will be used to only generate rooms with openings on the top
 			//########################################################################################
 		{
+			Coord.mapY = Coord.mapY + 1;
 			Player.yCoord = 1;
 			switch(w)
 			{
@@ -455,6 +532,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = false;
+				exitRight = false;
 				break;
 				}
 			case 1: {//initialize with walls on all sides but bottom and top have pathway
@@ -476,6 +557,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 2: {//initialize with walls on all sides but top and left have pathway
@@ -497,6 +582,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = false;
 				break;
 				}
 			case 3: {//initialize with walls on all sides but top and right have pathway
@@ -518,6 +607,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 4: {//initialize with walls on all sides but bottom, top, and right have pathway
@@ -543,6 +636,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 5: {//initialize with walls on all sides but bottom, top, and left have pathway
@@ -568,6 +665,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 6: {//initialize with walls on all sides but top, right, and left have pathway
@@ -593,6 +694,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = false;
+				exitRight = true;
 				break;
 				}
 			case 7: {//initialize with walls on all sides but all sides have pathway
@@ -622,6 +727,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			}
@@ -631,6 +740,7 @@ public class Room
 			//this will be used to only generate rooms with openings on the bottom
 			//########################################################################################
 		{
+			Coord.mapY = Coord.mapY - 1;
 			Player.yCoord = Main.MAP_HEIGHT-2;
 			switch(w)
 			{
@@ -649,6 +759,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 1: {//initialize with walls on all sides but bottom and top have pathway
@@ -670,6 +784,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 2: {//initialize with walls on all sides but bottom and left have pathway
@@ -691,6 +809,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 3: {//initialize with walls on all sides but bottom and right have pathway
@@ -712,6 +834,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 4: {//initialize with walls on all sides but bottom, top, and right have pathway
@@ -737,6 +863,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = false;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 5: {//initialize with walls on all sides but bottom, top, and left have pathway
@@ -762,6 +892,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = false;
 				break;
 				}
 			case 6: 	{//initialize with walls on all sides but bottom, right, and left have pathway
@@ -787,6 +921,10 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = false;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			case 7: {//initialize with walls on all sides but all sides have pathway
@@ -816,11 +954,16 @@ public class Room
 							map[x][y] = Main.BLOCK_DOOR;
 					}
 				}
+				exitTop = true;
+				exitLeft = true;
+				exitBottom = true;
+				exitRight = true;
 				break;
 				}
 			}
 		}
-	
+		currentRoomX = Coord.mapX;
+		currentRoomY = Coord.mapY;
 /*
 		switch(w)
 		{
